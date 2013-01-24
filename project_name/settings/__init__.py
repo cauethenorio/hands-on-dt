27391a71b8da8ac8297a88e1edf0650aba5817a9
Cauê Thenório <cauelt@gmail.com>
2013-01-24 17:22:05 -0200

    Initial commit

---
+++ project_name/settings/__init__.py
@@ -0,0 +1,4 @@
+# coding: utf-8
+
+from .general import *
+from .environment import *
---
+++ project_name/settings/environment.py
@@ -0,0 +1,21 @@
+# coding: utf-8
+# Local django settings for {{ project_name }} project.
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '',                      # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '',                      # Set to empty string for default.
+    }
+}
+
+HMAC_KEYS = {
+    '2000-01-01': '',
+}
